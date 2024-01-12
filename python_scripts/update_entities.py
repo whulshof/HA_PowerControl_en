6c90d68aa4752d9942aa29c48511c9ac093cb0fe
Willem Hulshof <whulshof@tosc.nl>
2024-01-13 00:41:46 +0100

    python scripts

--- python_scripts/update_entities.py
+++ python_scripts/update_entities.py
@@ -4,45 +4,45 @@ all_switches = ["Selected"]
 for e in entities:
     all_switches.append(e)
 
-service_data = {'entity_id': 'input_select.load_1_switch', 'options': sorted(all_switches)}
+service_data = {'entity_id': 'input_select.load_switch_1', 'options': sorted(all_switches)}
 hass.services.call('input_select', 'set_options', service_data)
-service_data = {'entity_id': 'input_select.load_2_switch', 'options': sorted(all_switches)}
+service_data = {'entity_id': 'input_select.load_switch_2', 'options': sorted(all_switches)}
 hass.services.call('input_select', 'set_options', service_data)
-service_data = {'entity_id': 'input_select.load_3_switch', 'options': sorted(all_switches)}
+service_data = {'entity_id': 'input_select.load_switch_3', 'options': sorted(all_switches)}
 hass.services.call('input_select', 'set_options', service_data)
-service_data = {'entity_id': 'input_select.load_4_switch', 'options': sorted(all_switches)}
+service_data = {'entity_id': 'input_select.load_switch_4', 'options': sorted(all_switches)}
 hass.services.call('input_select', 'set_options', service_data)
-service_data = {'entity_id': 'input_select.load_5_switch', 'options': sorted(all_switches)}
+service_data = {'entity_id': 'input_select.load_switch_5', 'options': sorted(all_switches)}
 hass.services.call('input_select', 'set_options', service_data)
-service_data = {'entity_id': 'input_select.load_6_switch', 'options': sorted(all_switches)}
+service_data = {'entity_id': 'input_select.load_switch_6', 'options': sorted(all_switches)}
 hass.services.call('input_select', 'set_options', service_data)
-service_data = {'entity_id': 'input_select.load_7_switch', 'options': sorted(all_switches)}
+service_data = {'entity_id': 'input_select.load_switch_7', 'options': sorted(all_switches)}
 hass.services.call('input_select', 'set_options', service_data)
-service_data = {'entity_id': 'input_select.load_8_switch', 'options': sorted(all_switches)}
+service_data = {'entity_id': 'input_select.load_switch_8', 'options': sorted(all_switches)}
 hass.services.call('input_select', 'set_options', service_data)
-service_data = {'entity_id': 'input_select.load_9_switch', 'options': sorted(all_switches)}
+service_data = {'entity_id': 'input_select.load_switch_9', 'options': sorted(all_switches)}
 hass.services.call('input_select', 'set_options', service_data)
-service_data = {'entity_id': 'input_select.load_10_switch', 'options': sorted(all_switches)}
+service_data = {'entity_id': 'input_select.load_switch_10', 'options': sorted(all_switches)}
 hass.services.call('input_select', 'set_options', service_data)
-service_data = {'entity_id': 'input_select.load_11_switch', 'options': sorted(all_switches)}
+service_data = {'entity_id': 'input_select.load_switch_11', 'options': sorted(all_switches)}
 hass.services.call('input_select', 'set_options', service_data)
-service_data = {'entity_id': 'input_select.load_12_switch', 'options': sorted(all_switches)}
+service_data = {'entity_id': 'input_select.load_switch_12', 'options': sorted(all_switches)}
 hass.services.call('input_select', 'set_options', service_data)
-service_data = {'entity_id': 'input_select.load_13_switch', 'options': sorted(all_switches)}
+service_data = {'entity_id': 'input_select.load_switch_13', 'options': sorted(all_switches)}
 hass.services.call('input_select', 'set_options', service_data)
-service_data = {'entity_id': 'input_select.load_14_switch', 'options': sorted(all_switches)}
+service_data = {'entity_id': 'input_select.load_switch_14', 'options': sorted(all_switches)}
 hass.services.call('input_select', 'set_options', service_data)
-service_data = {'entity_id': 'input_select.load_15_switch', 'options': sorted(all_switches)}
+service_data = {'entity_id': 'input_select.load_switch_15', 'options': sorted(all_switches)}
 hass.services.call('input_select', 'set_options', service_data)
-service_data = {'entity_id': 'input_select.load_16_switch', 'options': sorted(all_switches)}
+service_data = {'entity_id': 'input_select.load_switch_16', 'options': sorted(all_switches)}
 hass.services.call('input_select', 'set_options', service_data)
-service_data = {'entity_id': 'input_select.load_17_switch', 'options': sorted(all_switches)}
+service_data = {'entity_id': 'input_select.load_switch_17', 'options': sorted(all_switches)}
 hass.services.call('input_select', 'set_options', service_data)
-service_data = {'entity_id': 'input_select.load_18_switch', 'options': sorted(all_switches)}
+service_data = {'entity_id': 'input_select.load_switch_18', 'options': sorted(all_switches)}
 hass.services.call('input_select', 'set_options', service_data)
-service_data = {'entity_id': 'input_select.load_19_switch', 'options': sorted(all_switches)}
+service_data = {'entity_id': 'input_select.load_switch_19', 'options': sorted(all_switches)}
 hass.services.call('input_select', 'set_options', service_data)
-service_data = {'entity_id': 'input_select.load_20_switch', 'options': sorted(all_switches)}
+service_data = {'entity_id': 'input_select.load_switch_20', 'options': sorted(all_switches)}
 hass.services.call('input_select', 'set_options', service_data)
 
 entities = hass.states.entity_ids('sensor')
